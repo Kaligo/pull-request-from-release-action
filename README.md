d55e3da9fbb6237df8c4aa3c95f3121059cd3046
Alcedo Nathaniel De Guzman Jr <nathaniel.alcedo@ascendaloyalty.com>
2021-06-29 17:22:11 +0800

    Implement action

---
+++ README.md
@@ -0,0 +1,39 @@
+# pull-request-from-release-action
+
+This action helps to create a branch from a release and raises a pull request against a base branch.
+
+# Why?
+There are certain work flows where one may have a long lived branch that acts as a pre-production environment. Whenever a candidate release is created on Github, one may want to merge that release back
+into one or multiple `base-branches` and deploy that to an environment. This action automates that process.
+
+# Usage
+
+See [action.yml](action.yml)
+
+Basic:
+```yaml
+on:
+  release:
+    types:
+      - published
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      -  uses: actions/checkout@v2
+      -  uses: actions/setup-node@v1
+      -  uses: kaligo/pull-request-from-release-action@v1
+         with:
+           token: ${{ secrets.TOKEN }}
+           base-branches: |
+            'another-branch-uat
+             another-two-branch-uat'
+           title-suffix: 'Release UAT'
+           branch-suffix: '-UAT'
+           release-prefix: 'release'
+```
+
+# License
+
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
---
+++ index.js
@@ -0,0 +1,56 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  const token = core.getInput('token');
+  const baseBranches = core.getMultilineInput('base-branches');
+  const titleSuffix = core.getInput('title-suffix');
+  const branchSuffix = core.getInput('branch-suffix');
+  const releasePrefix = core.getInput('release-prefix');
+  const {payload: {repository, release}, sha} = github.context;
+  const { body, tag_name } = release;
+
+   /**
+   * Check if the release matches it's release-prefix tag
+   * If it does, that means this release is for the release prefix tag
+   */
+  if (!(tag_name).includes(releasePrefix)) {
+    process.exit(0);
+  }
+
+  /**
+   * 1. Create branch from release tag
+   * 2. Create pull request from the tag name and match it with base branch
+   */
+
+
+  const octokit = github.getOctokit(token);
+  const branchName = `refs/heads/_${tag_name}${branchSuffix}`;
+  const title = `${tag_name} ${titleSuffix}`;
+  const owner = repository.owner.login;
+  const repo = repository.name;
+
+  // Remove duplicate branches
+  const branches = Array.from(new Set(baseBranches));
+
+  await octokit.rest.git.createRef({
+    owner,
+    repo,
+    sha,
+    ref: branchName
+  });
+
+  for (const branch of branches) {
+    await octokit.rest.pulls.create({
+      owner,
+      repo,
+      title,
+      base: branch,
+      body,
+      head: branchName
+    })
+  }
+
+}
+
+run();
